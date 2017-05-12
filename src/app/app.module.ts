35b5bf71bb02c712a4af481146921b6e42feffbf
Sampath Kumar <sampathvrss@gmail.com>
2017-05-12 23:23:16 +0530

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,27 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RightComponent } from './login/right/right.component';
+import { LeftComponent } from './login/left/left.component';
+import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
+import {router} from './app.router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RightComponent,
+    LeftComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule,
+    RouterModule.forRoot(router)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.router.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+
+export const router: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent},
+  { path: 'home', component: HomeComponent},
+];
